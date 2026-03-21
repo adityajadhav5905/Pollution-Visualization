9f7303903ab4f288b6b0d371df0f8c4d548cefea
Aditya Jadhav <160160413+adityajadhav5905@users.noreply.github.com>
2026-03-21 20:50:59 +0530

    csdafvgbhkc

--- app.py
+++ app.py
@@ -650,4 +650,4 @@ def clear_files():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
